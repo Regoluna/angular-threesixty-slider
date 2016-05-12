238f2926449191543d4e76848d88e45eeacc44cd
Rodrigo Saiz <rodrigo@regoluna.com>
2016-05-12 15:32:10 +0200

    Removed extra files in demo

--- demo/scripts/app.js
+++ demo/scripts/app.js
@@ -9,4 +9,13 @@
  * Main module of the application.
  */
 angular
-  .module('threesixtyDemo', ['reg.threeSixty']);
+  .module('threesixtyDemo', ['reg.threeSixty'])
+    .controller('MainCtrl', [ '$scope', function ($scope) {
+
+      $scope.imageList = [];
+
+      for( var i=1; i<47; i++ ){
+        $scope.imageList.push( 'images/' + i + '.jpg' );
+      }
+
+    }]);
